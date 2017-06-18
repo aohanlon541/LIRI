961a2325db11a67fa9501b75d67653efa3a56dfd
Allison O'Hanlon <allison.t.ohanlon@gmail.com>
2017-06-17 20:30:18 -0500

    first attempt/going no where

---
+++ key.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+
+exports.twitterKeys = {
+  consumer_key: 'KvkvXU34ZOkqpOqmqzWluRaD0',
+  consumer_secret: 'mu5qfo4SCrmKnUDFjjvuPHxA1sJLbNWtm0rru9u6qXc5A9dFYf',
+  access_token_key: '35126132-JWKHhtWDEj56WuBQ21gmh1UiucD2IXJXGl3ldrGp',
+  access_token_secret: 'WYYX8TIo2HsuO1tHhZoczyhOwTR0e24LmhTh3LXubv2aC',
+  screen_name: 'alleeoo'
+}
---
+++ liri-bot.js
@@ -0,0 +1,45 @@
+var fs = require('fs');  
+// var twitterKeys = require('./key.js').twitterKeys;
+// var Twitter = require('twitter');
+
+//         var client = new Twitter({
+//             consumer_key: twitterKeys.consumer_key,
+//             consumer_secret: twitterKeys.consumer_secret,
+//             access_token_key: twitterKeys.access_token_key,
+//             access_token_secret: twitterKeys.access_token_secret
+//         });
+
+// var params = {screen_name: 'alleeoo'};
+// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//   if (!error) {
+//     console.log(response);
+//   }
+//   else {
+//       console.log("error");
+//   }
+// });
+
+
+var SpotifyWebApi = require('spotify-web-api-node');
+
+// credentials are optional
+var spotifyApi = new SpotifyWebApi({
+  clientId : '08a730439a40438798d9240f843da53a',
+  clientSecret : 'ab7c50a3a2ad430d91fd9297f9506432',
+});
+
+// Get an access token and 'save' it using a setter
+spotifyApi.clientCredentialsGrant()
+  .then(function(data) {
+    console.log('The access token is ' + data.body['access_token']);
+    spotifyApi.setAccessToken(data.body['access_token']);
+  }, function(err) {
+    console.log('Something went wrong!', err);
+  });
+
+spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
+  .then(function(data) {
+    console.log('Artist albums', data.body);
+  }, function(err) {
+    console.error(err);
+  });
