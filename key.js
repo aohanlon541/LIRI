d37c86a387ffc952305173d72366efa8027a1b0f
Allison O'Hanlon <allison.t.ohanlon@gmail.com>
2017-06-19 21:11:46 -0500

    twitter and movie functions work

--- key.js
+++ key.js
@@ -2,9 +2,9 @@ console.log('this is loaded');
 
 
 exports.twitterKeys = {
-  consumer_key: 'KvkvXU34ZOkqpOqmqzWluRaD0',
-  consumer_secret: 'mu5qfo4SCrmKnUDFjjvuPHxA1sJLbNWtm0rru9u6qXc5A9dFYf',
-  access_token_key: '35126132-JWKHhtWDEj56WuBQ21gmh1UiucD2IXJXGl3ldrGp',
+  consumer_key: 'A3neTMu7aBFEhPIXRhkSAfgWX',
+  consumer_secret: 'mILxngvQuXQjUy6IvnalYgU8G2fSt1EXs21MQ46hF7uoiXQUlF',
+  access_token_key: '35126132-2JWKHhtWDEj56WuBQ21gmh1UiucD2IXJXGl3ldrGp',
   access_token_secret: 'WYYX8TIo2HsuO1tHhZoczyhOwTR0e24LmhTh3LXubv2aC',
   screen_name: 'alleeoo'
 }
--- liri-bot.js
+++ liri-bot.js
@@ -1,45 +1,79 @@
 var fs = require('fs');  
-// var twitterKeys = require('./key.js').twitterKeys;
-// var Twitter = require('twitter');
+var twitterKeys = require('./key.js').twitterKeys;
+var Twitter = require('twitter');
+var command = process.argv[2];
+var searchTerm = process.argv[3];
 
-//         var client = new Twitter({
-//             consumer_key: twitterKeys.consumer_key,
-//             consumer_secret: twitterKeys.consumer_secret,
-//             access_token_key: twitterKeys.access_token_key,
-//             access_token_secret: twitterKeys.access_token_secret
-//         });
+if (command === "my-tweets") {
+    getTweets();
+}
 
-// var params = {screen_name: 'alleeoo'};
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//   if (!error) {
-//     console.log(response);
-//   }
-//   else {
-//       console.log("error");
-//   }
+// if (command === "spotify-this-song") {
+//     getSong();
+// }
+
+if (command === "movie-this") {
+    getMovie();
+}
+
+
+
+function getTweets() {
+        var client = new Twitter({
+            consumer_key: twitterKeys.consumer_key,
+            consumer_secret: twitterKeys.consumer_secret,
+            access_token_key: twitterKeys.access_token_key,
+            access_token_secret: twitterKeys.access_token_secret
+        });
+
+client.get('statuses/user_timeline', {
+    screen_name: 'alleeoo',
+    count: 20
+}, function(error, tweets, response) {
+  if (!error) {
+      for (i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].created_at + ": " + tweets[i].text);
+      }
+  }
+  else {
+      console.log("error");
+  }
+});
+}
+
+// function getSong() {
+// var Spotify = require('node-spotify-api');
+
+// var spotify = new Spotify({
+//   id: '08a730439a40438798d9240f843da53a',
+//   secret: 'e58416a56a2348eda029fe399b1e1759'
 // });
 
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
 
-var SpotifyWebApi = require('spotify-web-api-node');
+// console.log(data); 
+// });
 
-// credentials are optional
-var spotifyApi = new SpotifyWebApi({
-  clientId : '08a730439a40438798d9240f843da53a',
-  clientSecret : 'ab7c50a3a2ad430d91fd9297f9506432',
+function getMovie() {
+var request = require('request');
+
+var queryUrl = "http://www.omdbapi.com/?apikey=40e9cece&t=" + searchTerm;
+
+request(queryUrl, function(error, response, body){
+    if (!error && response.statusCode === 200) {
+        var parsedResults = JSON.parse(body);
+        console.log("* " + parsedResults.Title);
+        console.log("* " + parsedResults.Year);
+        console.log("* IMDB Rating: " + parsedResults.imdbRating);
+        console.log("* Country: " + parsedResults.Country);
+        console.log("* Lang: " + parsedResults.Language);
+        console.log("* " + parsedResults.Plot);
+        console.log("* Actors: " + parsedResults.Actors);
+        console.log("* " + parsedResults.Ratings[1].Source + ": " + parsedResults.Ratings[1].Value);
+
+    }
 });
-
-// Get an access token and 'save' it using a setter
-spotifyApi.clientCredentialsGrant()
-  .then(function(data) {
-    console.log('The access token is ' + data.body['access_token']);
-    spotifyApi.setAccessToken(data.body['access_token']);
-  }, function(err) {
-    console.log('Something went wrong!', err);
-  });
-
-spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE')
-  .then(function(data) {
-    console.log('Artist albums', data.body);
-  }, function(err) {
-    console.error(err);
-  });
+}
